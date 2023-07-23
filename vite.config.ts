078a8a663535a656a10176bf910ec08d6e8a1886
galinor <ali.atguedda@gmail.com>
2023-07-24 00:10:50 +0100

    project init + lib config

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'vue-input-highlighter',
+      fileName: (format) => `${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    },
+  },
+  plugins: [vue()],
+})
